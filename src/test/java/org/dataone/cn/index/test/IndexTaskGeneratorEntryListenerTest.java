e0eb97b958b20a5bc0d17667dca6e1adf319fd0f
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-11-09 15:21:22 +0000

    upgrading index components to hazelcast 2.4.1-SNAPSHOT

--- src/test/java/org/dataone/cn/index/test/IndexTaskGeneratorEntryListenerTest.java
+++ src/test/java/org/dataone/cn/index/test/IndexTaskGeneratorEntryListenerTest.java
@@ -82,7 +82,7 @@ public class IndexTaskGeneratorEntryListenerTest {
             System.out.print(mapName + " ");
         }
         System.out.println();
-        hzMember = Hazelcast.init(hzConfig);
+        hzMember = Hazelcast.newHazelcastInstance(hzConfig);
         System.out.println("Hazelcast member hzMember name: " + hzMember.getName());
 
         sysMetaMap = hzMember.getMap(systemMetadataMapName);
